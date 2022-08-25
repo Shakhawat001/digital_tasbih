dd2302e413e6d139e4078a3c6eed92e33f60ed51
shakhawat001 <mattrixshakhawat@gmail.com>
2022-08-25 15:29:58 +0600

    2nd commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ## My very First Flutter Project
+
+Basically, It's another counter app.
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'dart:ui';
+import 'dart:io' show platform;
 import 'package:flutter/material.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
+
   runApp(const MyApp());
 }
 
@@ -11,11 +17,46 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      themeMode: ThemeMode.system,
+      darkTheme: ThemeData(
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.black,
+          foregroundColor: Colors.white,
+        ),
+        scaffoldBackgroundColor: Colors.black87,
+        primarySwatch: Colors.teal,
         useMaterial3: true,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            primary: Colors.teal,
+            onPrimary: Colors.black87,
+            // padding: EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+            textStyle: TextStyle(
+              fontSize: 12,
+            ),
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
+        primarySwatch: Colors.teal,
+        useMaterial3: true,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.black,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            primary: Colors.teal,
+            onPrimary: Colors.white,
+            // padding: EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+            textStyle: TextStyle(
+              fontSize: 12,
+            ),
+          ),
+        ),
+      ),
+      home: const MyHomePage(title: 'Digital Tasbih'),
     );
   }
 }
@@ -31,6 +72,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  List<bool> isSelected = [true, false];
 
   void _incrementCounter() {
     setState(() {
@@ -38,30 +80,82 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void counterReset() {
+    setState(() {
+      _counter = 0;
+    });
+  }
+
+  AdRequest? adRequest;
+  BannerAd? bannerAdd;
+
+  @override
+  void initState() {
+    super.initState();
+
+    String bannerID = "ca-app-pub-3940256099942544/6300978111";
+
+    adRequest = const AdRequest(
+      nonPersonalizedAds: false,
+    );
+
+    BannerAdListener bannerAdListener = BannerAdListener(onAdClosed: (ad) {
+      bannerAdd!.load();
+    }, onAdFailedToLoad: (ad, err) {
+      bannerAdd!.load();
+    });
+
+    bannerAdd = BannerAd(
+        size: AdSize.banner,
+        adUnitId: bannerID,
+        listener: bannerAdListener,
+        request: adRequest!);
+
+    bannerAdd!.load();
+  }
+
+  @override
+  void dispose() {
+    bannerAdd!.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("Digital Tasbih"),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                AdWidget(
+                  ad: bannerAdd!,
+                ),
+                ElevatedButton.icon(
+                  onPressed: counterReset,
+                  label: Text("Reset"),
+                  icon: Icon(Icons.restart_alt_rounded),
+                ),
+              ],
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+              style: TextStyle(color: Colors.teal, fontSize: 72),
+            ),
+            ElevatedButton(
+              onPressed: _incrementCounter,
+              child: Icon(Icons.add),
+              style: ButtonStyle(
+                padding: MaterialStateProperty.all(const EdgeInsets.all(50)),
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
